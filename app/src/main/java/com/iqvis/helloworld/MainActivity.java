24a8839f823b0483ede0774186e27e4a09712dc0
Naeems <naeems@live.com>
2017-04-29 00:21:52 +0500

    Build is changed a bit

--- app/src/main/java/com/iqvis/helloworld/MainActivity.java
+++ app/src/main/java/com/iqvis/helloworld/MainActivity.java
@@ -1,7 +1,9 @@
 package com.iqvis.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.testfairy.TestFairy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestFairy.begin(this, "3dde51e1d2d95ae6d8d1a36d423078e3eba65f9f");
     }
 }
